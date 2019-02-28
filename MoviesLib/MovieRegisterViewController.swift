a8d6addb9e5da1c91f11887987a78fe882a2420b
Usuário Convidado <Guest@pa06micro33.local>
2019-02-27 21:57:05 -0300

    implements trailer feature

--- MoviesLib/MovieRegisterViewController.swift
+++ MoviesLib/MovieRegisterViewController.swift
@@ -20,6 +20,7 @@ class MovieRegisterViewController: UIViewController {
     @IBOutlet weak var tvSummary: UITextView!
     
     @IBOutlet weak var btAddEdit: UIButton!
+    @IBOutlet weak var ivPoster: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,27 +31,66 @@ class MovieRegisterViewController: UIViewController {
             tfDuration.text = movie.duration
             tfCategories.text = movie.categories
             tvSummary.text = movie.summary
-            btAddEdit.setTitle("Älterar", for: .normal)
+            ivPoster.image = movie.image as? UIImage
+            btAddEdit.setTitle("Alterar", for: .normal)
+            
         }
     }
     
     @IBAction func addEditMovie(_ sender: UIButton) {
         if movie == nil {
             movie = Movie(context: context)
-            movie?.title = tfTitle.text
-            movie?.rating = Double(tfRating.text!) ?? 0
-            movie?.categories = tfCategories.text
-            movie?.duration = tfDuration.text
-            movie?.summary = tvSummary.text
+        }
+        movie?.title = tfTitle.text
+        movie?.rating = Double(tfRating.text!) ?? 0
+        movie?.categories = tfCategories.text
+        movie?.duration = tfDuration.text
+        movie?.summary = tvSummary.text
+        movie?.image = ivPoster.image
+        do {
+            try context.save()
+            navigationController?.popViewController(animated: true)
+        } catch {
             
-            do {
-                try context.save()
-                navigationController?.popViewController(animated: true)
-            } catch {
-                
-            }
         }
         
     }
+    
+    func selectPicture(source: UIImagePickerController.SourceType){
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = source
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func selectPoster(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar pôster", message: "De onde você quer escolher o pôster?", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Câmera", style: .default) { (_) in
+            self.selectPicture(source: .camera)
+        }
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de Fotos", style: .default) { (_) in
+            self.selectPicture(source: .photoLibrary)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel) { (_) in
+            
+        }
+        
+        alert.addAction(cameraAction)
+        alert.addAction(libraryAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
 
+extension MovieRegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            ivPoster.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
--- MoviesLib/MovieViewController.swift
+++ MoviesLib/MovieViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import AVKit
 
 class MovieViewController: UIViewController {
     
+    //MARK: - Properties -
     var movie: Movie?
+    var trailer: String = ""
+    var moviePlayer: AVPlayer?
+    var moviePlayerController: AVPlayerViewController?
     
+    //MARK: - IBOutlets -
     @IBOutlet weak var ivPoster: UIImageView!
     @IBOutlet weak var lbTitle: UILabel!
     @IBOutlet weak var lbDuration: UILabel!
@@ -19,11 +25,17 @@ class MovieViewController: UIViewController {
     @IBOutlet weak var lbCategories: UILabel!
     @IBOutlet weak var tvSummary: UITextView!
     
+    //MARK: - Super Methods -
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let title = movie?.title {
+            loadTrailer(title: title)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         if let movie = movie {
-            //ivPoster.image = UIImage(named: movie.image)
+            ivPoster.image = movie.image as? UIImage
             lbTitle.text = movie.title
             lbDuration.text = movie.duration
             lbRating.text = "⭐️ \(movie.rating)/10"
@@ -32,6 +44,36 @@ class MovieViewController: UIViewController {
             
         }
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? MovieRegisterViewController {
+            vc.movie = movie
+        }
+    }
+    
+    //MARK: - Methods -
+    func loadTrailer(title: String){
+        API.loadTrailers(title: title) { (apiResult) in
+            guard let results = apiResult?.results, let trailer = results.first else {return}
+            self.trailer = trailer.previewUrl
+            DispatchQueue.main.async {
+                self.prepareVideo()
+            }
+        }
+    }
+    
+    func prepareVideo(){
+        guard let url = URL(string: trailer) else {return}
+        moviePlayer = AVPlayer(url: url)
+        moviePlayerController = AVPlayerViewController()
+        moviePlayerController?.player = moviePlayer
+        moviePlayerController?.showsPlaybackControls = true
+    }
+    
+    //MARK: - Actions -
+    @IBAction func playMovieTrailer(_ sender: UIButton) {
+        guard let moviePlayerController = moviePlayerController else {return}
+        present(moviePlayerController, animated: true, completion: nil)
+    }
 }
 
